f167108465e36841bf4585221a1b497eab40f4af
SamFCLeite <leite.samuelfc@gmail.com>
2026-09-05 23:10:38 -0300

    Cadastro de banco com deposito e saque

---
+++ CadastroBanco/CadastroBanco/ContaBancaria.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadastroBanco {
+    internal class ContaBancaria {
+        //DADOS PRINCIPAIS
+        public int NumeroConta {get; private set; } //PODE RECEBER VALOR, MAS NÃO PODE ALTERAR
+        public String NomeUsuario { get; set; }
+        public double Deposito { get; private set; } //PODE RECEBER VALOR, MAS NÃO PODE ALTERAR
+
+        //MINIMO DE DADOS
+        public ContaBancaria(int numeroConta, string nomeUsuario) {
+            NumeroConta = numeroConta;
+            NomeUsuario = nomeUsuario;
+            Deposito = 0;
+        }
+
+        //TODOS OS DADOS
+        public ContaBancaria(int numeroConta, string nomeUsuario, double deposito) { 
+            NumeroConta = numeroConta;
+            NomeUsuario = nomeUsuario;
+            Deposito = deposito;
+        }
+
+        //FUNÇÕES DE DEPÓSITO E SAQUE
+        public void Depositar(double valor) {
+            if(valor < 0) {
+                valor = 0;
+            }
+            Deposito += valor;
+        }
+
+        public void Sacar(double valor) {
+            if(valor < 0) {
+                valor = 0;
+            }
+            Deposito -= valor + 5.00;
+        }
+
+        //CONVERTENDO INFORMAÇÕES EM STRING PARA IMPRESSÃO
+        public override string ToString() {
+            return "Conta: " + NumeroConta + ", Titular: " + NomeUsuario + ", Saldo: R$ " + Deposito.ToString("F2");
+        }
+
+    }
+}
---
+++ CadastroBanco/CadastroBanco/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace CadastroBanco {
+    internal class Program {
+        static void EscreverDadosConta(ContaBancaria usuario) {
+            Console.WriteLine();
+            Console.WriteLine("Dados da Conta: ");
+            Console.WriteLine(usuario);
+        }
+        static void Main(string[] args) {
+            ContaBancaria usuario;
+
+            //ENTRANDO COM OS DADOS DO USUÁRIO
+            Console.Write("Entre o número da conta: ");
+            int NumeroConta = int.Parse(Console.ReadLine());
+
+            Console.Write("Entre o nome do titular: ");
+            String NomeUsuario = Console.ReadLine();
+
+            Console.Write("Haverá depósito inicial (s/n)?: ");
+            String resposta = Console.ReadLine();
+            if(resposta == "s") {
+                Console.Write("Entre o valor de depósito inicial: ");
+                double deposito = double.Parse(Console.ReadLine());
+                usuario = new ContaBancaria(NumeroConta, NomeUsuario, deposito);
+            } else {
+                usuario = new ContaBancaria(NumeroConta, NomeUsuario);
+            }
+
+            //ESCREVENDO OS DADOS DA CONTA
+            EscreverDadosConta(usuario);
+
+            //DEPÓSITO
+            Console.WriteLine();
+            Console.WriteLine("Entre um valor para depósito: ");
+            double valorDeposito = double.Parse(Console.ReadLine());
+            usuario.Depositar(valorDeposito);
+
+            //ESCREVENDO OS DADOS DA CONTA APÓS O DEPÓSITO
+            EscreverDadosConta(usuario);
+
+            //SAQUE
+            Console.WriteLine();
+            Console.WriteLine("Entre um valor para saque: ");
+            double valorSaque = double.Parse(Console.ReadLine());
+            usuario.Sacar(valorSaque);
+
+            //ESCREVENDO OS DADOS DA CONTA APÓS O SAQUE
+            //teste
+            EscreverDadosConta(usuario);
+        }
+    }
+}
